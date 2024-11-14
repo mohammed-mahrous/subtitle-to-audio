35b81ca37d92ff32fedaeeb7e466a55eb9bcf81f
mohamed-mahrous <xxspider6@gmail.com>
2024-11-14 14:51:09 +0200

    check speakers

--- TransformersProcessor.py
+++ TransformersProcessor.py
@@ -15,10 +15,10 @@ class TransformersProcessor:
         audio_array_from_text = self.model.generate(**text_inputs, tgt_lang="arb",speaker_id=10)[0].cpu().numpy().squeeze()
         return audio_array_from_text.astype(np.float32)
 
-    def ProcessAndWriteFile(self,message:str, file_path:str="out_from_text.wav") -> None:
+    def ProcessAndWriteFile(self,message:str, file_path:str="out_from_text.wav", speakerId:int = 0) -> None:
         
         text_inputs = self.processor(text = message, src_lang="arb", return_tensors="pt").to(self.device)
-        audio_array_from_text = self.model.generate(**text_inputs, tgt_lang="arb",speaker_id=10)[0].cpu().numpy().squeeze()
+        audio_array_from_text = self.model.generate(**text_inputs, tgt_lang="arb",speaker_id=speakerId)[0].cpu().numpy().squeeze()
         sample_rate = self.model.config.sampling_rate
         scipy.io.wavfile.write(file_path, rate=16000, data=audio_array_from_text.astype(np.float32))
 
--- subtitle_to_audio.py
+++ subtitle_to_audio.py
@@ -5,54 +5,54 @@ from pysubparser import parser
 from pydub import AudioSegment
 from TransformersProcessor import TransformersProcessor as tts
 
+speakersIds = [i for i in range(0,11)]
+
 def time_to_ms(time):
   return ((time.hour * 60 + time.minute) * 60 + time.second) * 1000 + time.microsecond / 1000
 
-def generate_audio(path):  
+def generate_audio(path:str, speakerId:int = 0):  
   print("Generating audio file for {} with {}".format(path, "transformers"))      
 
   subtitles = parser.parse(path)
 
   tts_engine = tts()
   
-  audio_sum = AudioSegment.empty()   
-  
-  tempDir = "test/tempFolder"
-  if not os.path.isdir(tempDir):
-    os.mkdir(tempDir)
-      
-  print('Created temporary directory', tempDir)            
-  iterNum = 0
-  temp_file_path = os.path.join(tempDir, "temp.wav")
-  prev_subtitle = None
-  prev_audio_duration_ms = 0
-  for subtitle in subtitles:
-    iterNum = iterNum +1
-    temp_file_path = os.path.join(tempDir, f"temp{iterNum}.wav")   
-    tts_engine.ProcessAndWriteFile(subtitle.text, temp_file_path)
+  audio_sum = AudioSegment.empty()
 
-    audio_segment = AudioSegment.from_wav(temp_file_path)         
+  with tempfile.TemporaryDirectory() as tempDir:
+    print('Created temporary directory', tempDir)            
+    iterNum = 0
+    temp_file_path = os.path.join(tempDir, "temp.wav")
+    prev_subtitle = None
+    prev_audio_duration_ms = 0
+    for subtitle in subtitles:
+      iterNum = iterNum +1   
+      tts_engine.ProcessAndWriteFile(subtitle.text, temp_file_path, speakerId=speakerId)
 
-    print(subtitle.start, subtitle.text)
-      
-    if prev_subtitle is None:
-        silence_duration_ms = time_to_ms(subtitle.start)
-    else:
-      silence_duration_ms = time_to_ms(subtitle.start) - time_to_ms(prev_subtitle.start) - prev_audio_duration_ms
+      audio_segment = AudioSegment.from_wav(temp_file_path)         
 
-    audio_sum = audio_sum + AudioSegment.silent(duration=silence_duration_ms) + audio_segment                   
-      
-    prev_subtitle = subtitle
-    prev_audio_duration_ms = len(audio_segment)
+      print(subtitle.start, subtitle.text)
+        
+      if prev_subtitle is None:
+          silence_duration_ms = time_to_ms(subtitle.start)
+      else:
+        silence_duration_ms = time_to_ms(subtitle.start) - time_to_ms(prev_subtitle.start) - prev_audio_duration_ms
 
-    with open(os.path.splitext(path)[0] + '.wav', 'wb') as out_f:
-      audio_sum.export(out_f, format='wav')      
+      audio_sum = audio_sum + AudioSegment.silent(duration=silence_duration_ms) + audio_segment                   
+        
+      prev_subtitle = subtitle
+      prev_audio_duration_ms = len(audio_segment)
+
+      with open(os.path.splitext(path)[0] + '.wav', 'wb') as out_f:
+        audio_sum.export(out_f, format='wav')      
 
 if __name__ == "__main__":      
   arg_parser = argparse.ArgumentParser()
   arg_parser.add_argument("-p", "--path", help="subtitle file path", required=True)
   
   args = arg_parser.parse_args()
-  
-  generate_audio(path=args.path)    
+  for id in speakersIds:
+
+    generate_audio(path=args.path,speakerId=id)
+    print(f"finished for speaker {id}")  
 
